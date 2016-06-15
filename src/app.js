edaf02254b21b2e41cda166f1cf79f102aa6c093
alvarollmenezes <alvarollmenezes@gmail.com>
2016-06-15 17:46:24 -0300

    Novos endpoints de noticia e lista.

--- src/app.js
+++ src/app.js
@@ -11,11 +11,37 @@ app.use(function (req, res, next) {
 
 app.get('/destaques', function (req, res) {
 
-    var destaques = db.destaques;
+    let destaques = db.noticias;
 
-    res.json(destaques);
+    noticias.map(a => {
+        delete a.corpo;
+    });
+
+    return res.json(destaques);
 });
+
+
+app.get('/noticia/:id', (req, res) => {
+
+    let noticia = db.noticias.filter(a => a.id == req.params.id)[0];
+
+    delete noticia.resumo;
+
+    return res.json(noticia);
+});
+
+app.get('/lista', (req, res) => {
+
+    let noticias = db.noticias;
+
+    noticias.map(a => {
+        delete a.corpo;
+        delete a.resumo;
+    });
+
+    return res.json(noticias);
+});
+
 
 // Launch server
 app.listen(4243);
-
