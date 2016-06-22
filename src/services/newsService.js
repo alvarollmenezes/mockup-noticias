4adb17de013763feacc094394f1d1166c0e436c3
alvarollmenezes <alvarollmenezes@gmail.com>
2016-06-22 15:45:31 -0300

    Changed elasticsearch endpoint

--- src/services/newsService.js
+++ src/services/newsService.js
@@ -8,7 +8,7 @@ const highlightsEndpoint = `${orchardApi}noticias/Getdestaques`;
 
 // ElasticSearch
 const client = new elasticsearch.Client({
-    host: 'http://10.243.9.2',
+    host: 'http://10.243.9.4',
     log: 'error'
 });
 const newsIndex = 'news';
