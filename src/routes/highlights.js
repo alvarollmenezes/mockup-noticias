66aa4ebaf77c436705b86e9f224109a2a2f869e6
alvarollmenezes <alvarollmenezes@gmail.com>
2016-07-01 11:30:34 -0300

    Cache em memória nos acessos ao orchard

--- src/routes/highlights.js
+++ src/routes/highlights.js
@@ -1,8 +1,10 @@
+const apicache = require('apicache').options({ debug: false }).middleware;
+
 module.exports = app => {
 
     const highlightsController = require('../controllers/highlightsController')();
 
-    app.get('/highlights', highlightsController.getList);
+    app.get('/highlights', apicache('10 minutes'), highlightsController.getList);
 
     return app;
 };
