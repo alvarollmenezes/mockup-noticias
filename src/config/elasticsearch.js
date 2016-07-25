b386891caa45c96451855afbd54356f9d752fb96
alvarollmenezes <alvarollmenezes@gmail.com>
2016-07-25 17:29:41 -0300

    Passou o Lint

--- src/config/elasticsearch.js
+++ src/config/elasticsearch.js
@@ -1,13 +1,15 @@
-const elasticsearch = require('elasticsearch');
+const elasticsearch = require( 'elasticsearch' );
 
 const es_host = process.env.ELASTICSEARCH || 'http://10.243.9.4';
 
 module.exports = {
     newsIndex: 'news',
     newsType: 'news',
+    highlightsIndex: 'highlights',
+    highlightsType: 'highlights',
     host: es_host,
-    client: new elasticsearch.Client({
+    client: new elasticsearch.Client( {
         host: es_host,
         log: 'error'
-    })
+    } )
 };
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,54 +1,54 @@
-const newsService = require('../services/newsService')();
+const newsService = require( '../services/newsService' )();
 
 module.exports = () => {
     var homeController = new Object();
 
-    function normalizeArrayParameter(arrayParam) {
-        if (!arrayParam) {
+    function normalizeArrayParameter( arrayParam ) {
+        if ( !arrayParam ) {
             origins = [];
         }
-        else if (!Array.isArray(arrayParam)) {
-            origins = [arrayParam];
+        else if ( !Array.isArray( arrayParam ) ) {
+            origins = [ arrayParam ];
         }
         else {
             origins = arrayParam;
         }
 
         // Remove duplicates
-        return Array.from(new Set(origins));
+        return Array.from( new Set( origins ) );
     }
 
-    homeController.getList = (req, res) => {
+    homeController.getList = ( req, res ) => {
 
-        const dateMin = req.query.dateMin ? new Date(req.query.dateMin) : null;
-        let dateMax = req.query.dateMax ? new Date(req.query.dateMax) : null;
-        if (dateMax) {
+        const dateMin = req.query.dateMin ? new Date( req.query.dateMin ) : null;
+        let dateMax = req.query.dateMax ? new Date( req.query.dateMax ) : null;
+        if ( dateMax ) {
             // Get entire day
-            dateMax = dateMax.setDate(dateMax.getDate() + 1) - 1;
+            dateMax = dateMax.setDate( dateMax.getDate() + 1 ) - 1;
         }
         const query = req.query.query;
-        const origins = normalizeArrayParameter(req.query.origins);
+        const origins = normalizeArrayParameter( req.query.origins );
         const pageNumber = req.query.pageNumber || 1;
         const pageSize = req.query.pageSize || 10;
 
-        newsService.getList(dateMin, dateMax, query, origins, pageNumber, pageSize)
-            .then(news => {
-                return res.json(news);
-            })
-            .catch(err => {
+        newsService.getList( dateMin, dateMax, query, origins, pageNumber, pageSize )
+            .then( news => {
+                return res.json( news );
+            } )
+            .catch( err => {
                 throw err;
-            });
+            } );
     };
 
-    homeController.getSingle = (req, res) => {
+    homeController.getSingle = ( req, res ) => {
 
-        newsService.getSingle(req.params.id)
-            .then(news => {
-                return res.json(news);
-            })
-            .catch(err => {
+        newsService.getSingle( req.params.id )
+            .then( news => {
+                return res.json( news );
+            } )
+            .catch( err => {
                 throw err;
-            });
+            } );
     };
 
     return homeController;
