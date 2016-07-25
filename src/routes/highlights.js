e37de10f64e85509914a4c4c8f6c4faaf5c78945
alvarollmenezes <alvarollmenezes@gmail.com>
2016-07-25 17:59:50 -0300

    Remove cache dos destaques

--- src/routes/highlights.js
+++ src/routes/highlights.js
@@ -1,10 +1,8 @@
-const apicache = require('apicache').options({ debug: false }).middleware;
-
 module.exports = app => {
 
     const highlightsController = require( '../controllers/highlightsController' )();
 
-    app.get('/highlights', apicache('10 minutes'), highlightsController.getList);
+    app.get('/highlights', highlightsController.getList);
 
     return app;
 };
