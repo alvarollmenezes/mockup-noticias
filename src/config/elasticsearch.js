7995a697e26cdc71856d9fa8cfdee8b4509e2353
Alvaro Menezes <alvarollmenezes@gmail.com>
2019-10-30 17:37:59 -0300

    fix(elasticsearch): update version

--- src/config/elasticsearch.js
+++ src/config/elasticsearch.js
@@ -3,12 +3,12 @@ const elasticsearch = require( 'elasticsearch' );
 const es_host = process.env.ELASTICSEARCH || 'http://10.243.9.4';
 
 module.exports = {
-    newsIndex: 'news',
-    newsType: 'news',
-    highlightsIndex: 'highlights',
-    highlightsType: 'highlights',
-    lastUpdateIndex: 'news-last-update',
-    lastUpdateType: 'news-last-update',
+    newsIndex: process.env.NEWS_INDEX || 'espm-news',
+    newsType: process.env.NEWS_TYPE || 'news',
+    highlightsIndex: process.env.HIGHLIGHTS_INDEX || 'espm-highlights',
+    highlightsType: process.env.HIGHLIGHTS_TYPE || 'highlights',
+    lastUpdateIndex: process.env.LAST_UPDATE_INDEX || 'espm-news-last-update',
+    lastUpdateType: process.env.LAST_UPDATE_TYPE || 'news-last-update',
     host: es_host,
     client: new elasticsearch.Client( {
         host: es_host,
